986c043a4adf7e5b0c3feada79b9869216472ca9
Teddy Heinen <skyshayde@gmail.com>
2018-01-23 16:28:09 -0600

    move configure to separate script

--- install.sh
+++ install.sh
@@ -29,36 +29,14 @@ check_preinstall() {
 	fi
 }
 
-configure_in_chroot() {
-	ln -sf "/usr/share/zoneinfo/${TIME}" /etc/localtime
-	hwclock --systohc
-
-    echo 'LANG="en_US.UTF-8"' >> /etc/locale.conf
-    echo 'LC_COLLATE="C"' >> /etc/locale.conf
-    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-    locale-gen
-
-    echo "$hostname" > /etc/hostname
-
-    cat > /etc/hosts <<EOF
-127.0.0.1 localhost.localdomain localhost $hostname
-::1       localhost.localdomain localhost $hostname
-EOF
-	
-	pacman --noconfirm -S grub efibootmgr
-
-	grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
-	grub-mkconfig -o /boot/grub/grub.cfg
-	exit
-}
-
-configure() {
+install_arch() {
 	timedatectl set-ntp true
 	pacstrap /mnt base
 	genfstab -U /mnt >> /mnt/etc/fstab
-	arch-chroot /mnt /bin/bash -c  "configure_in_chroot"
+	wget https://raw.githubusercontent.com/Skyshayde/Arch-Install/master/configure.sh configure.sh
+	arch-chroot /mnt ./configure.sh
 }
 check_preinstall
 make_partitions
 mount_partitions
-configure
+install_arch
